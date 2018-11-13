cbb3411bd9158dd2b97cea8883051ab5282b5658
Micah Smith <micahsmith.92@gmail.com>
2018-11-12 23:08:29 -0500

    Added skeleton of express backend

---
+++ backend.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let path = require('path');
+let bodyparser = require('body-parser');
+
+let port = 8080;
+let app = express();
+
+// Required so that express can read POST data
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Simple test array to store appointments
+// Will be substituted with database later but for now will suffice
+let appointments = [{"name": "Test"}];
+
+app.route('/')
+      .get((req, res) => {
+        res.sendFile(path.join(__dirname + '/index.html'));
+      })
+
+app.route('/appointments')
+      // GET REQUEST
+      .get((req, res) => {
+        res.json(appointments);
+      })
+      // POST REQUEST
+      .post((req, res) => {
+        appointments.push(req.body.appointment);
+        res.send("Added " + req.body.appointment);
+      })
+
+app.listen(port, () => {
+        console.log("Running backend");
+      });
